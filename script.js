8c2259662fc903f0956ea31c8a87afccdc13a27c
Daiaaf <daiaaf@live.com>
2025-12-02 15:31:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Site carregado com sucesso!');
+const animated = document.querySelectorAll('.fade-in');
+const obs = new IntersectionObserver(e=>{
+    e.forEach(el=>{
+        if(el.isIntersecting) el.target.classList.add('show');
+    })
+},{threshold:.2});
+animated.forEach(a=>obs.observe(a));
